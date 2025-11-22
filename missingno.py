5bb284ec0acb6062207a7e4b679ab7573a432a7e
Ameersha1007 <ameersha1007@gmail.com>
2025-11-22 22:40:43 +0530

    missing no in array

--- missingno.py
+++ missingno.py
@@ -1,1 +1,12 @@
+n=5
 
+actualsum=0
+for i in arr:
+    actualsum=actualsum+i
+
+expectedsum=0
+for j in range(1,n+1):
+    expectedsum=expectedsum+j
+    
+missingno=expectedsum - actualsum
+print(missingno)
