c7166948d38bbd006bcc8e9f870a6a4fefb3e0cf
Ameersha1007 <ameersha1007@gmail.com>
2025-11-22 22:50:04 +0530

    count vowels&cons.

--- vowels&cons.py
+++ vowels&cons.py
@@ -1,1 +1,14 @@
+text=input("enter a string:")
+vowels="aeiou"
 
+vcount=0
+cocount=0
+
+for i in text:
+    if i.isalpha():
+        if i in vowels:
+            vcount=vcount+1
+        else :
+            cocount=cocount+1
+print(vcount)
+print(cocount)
