30a5809ea0aaed15518e4a5317f8fd262c06b6f0
Ameersha1007 <ameersha1007@gmail.com>
2025-11-22 22:30:50 +0530

    remove duplicate elements

--- duplicate.py
+++ duplicate.py
@@ -1,1 +1,17 @@
+arr=[2,3,5,7,2,3,5]
+arr2=[]
+
+for i in arr:
+    found=0
+
+    for j in arr2:
+        if j==i:
+            found=1
+
+    if found==0:
+         arr2.append(i)
+print(arr2)         
+        
+
+
 
